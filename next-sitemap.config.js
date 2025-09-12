2a39663119c4895979989d201e1db6f7d6ce8712
Anand K Hari <anandkhari145@gmail.com>
2025-09-12 16:15:39 +0530

    Add next-sitemap config and update build scripts

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://indousnexusllp.com",
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "weekly",
+  priority: 0.7,
+};
